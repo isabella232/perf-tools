7ab88f6222f05b314e3547a6a5470b5289939b74
GauravKolapate <gaurav.kolapate@quantiphi.com>
2021-01-06 23:58:01 +0530

    Added license in python scripts

--- Usecase 1/pipeline_automation_scripts/sqoop_job/sqoop_job_submit.py
+++ Usecase 1/pipeline_automation_scripts/sqoop_job/sqoop_job_submit.py
@@ -1,18 +1,19 @@
-/*
- * Copyright © 2018 Cask Data, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  Copyright © 2016 Cask Data, Inc.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
 
 import random
 import re
@@ -93,6 +94,7 @@ def submit_job(project_id, region, cluster_name, jdbc_string, gcs_bucket, mapper
     print("Sqoop job command is : {}".format(job))
     try: 
         logger.info("Starting sqoop job...%")
+        #submit sqoop job to dataproc cluster
         operation = job_client.submit_job_as_operation(
         request={"project_id": project_id, "region": region, "job": job}
     )
